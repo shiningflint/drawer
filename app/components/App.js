3db56467f0436ace5ae21a2fa0f7315535efff68
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-08-30 18:20:04 +0900

    refactor the menu code

--- app/components/App.js
+++ app/components/App.js
@@ -2,35 +2,35 @@ var React = require('react');
 var MenuList = require('./MenuList');
 var InfoWindow = require('./InfoWindow');
 
-var infos = {
-  info1: [
+var infos = [
+  [
     "https://source.unsplash.com/kUUacyTdChM/1024x768",
     "#c84d4b",
     "The Smell of a Coffee Dripper"
   ],
-  info2: [
+  [
     "https://source.unsplash.com/AETe6TuEBck/1024x768",
     "#634bc8",
     "The Eerie Silent of the Big City"
   ],
-  info3: [
+  [
     "https://source.unsplash.com/vM97bob4ZGk/1024x768",
     "#c8ac4b",
     "City Race"
   ],
-  info4: [
+  [
     "https://source.unsplash.com/lcg5PdLCOWs/1024x768",
     "#3d9e4d",
     "The Fear of God is the First Step"
   ]
-};
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      info_state: infos.info1,
+      info_state: 2,
       info_window: false
     }
 
@@ -39,27 +39,10 @@ class App extends React.Component {
   }
 
   handleClick(e) {
-    if (e.target.innerHTML === "1") {
-      this.setState({
-        info_state: infos.info1,
-        info_window: true
-      });
-    } else if (e.target.innerHTML === "2") {
-      this.setState({
-        info_state: infos.info2,
-        info_window: true
-      });
-    } else if (e.target.innerHTML === "3") {
-      this.setState({
-        info_state: infos.info3,
-        info_window: true
-      });
-    } else {
-      this.setState({
-        info_state: infos.info4,
-        info_window: true
-      });
-    }
+    this.setState({
+      info_state: e.target.innerHTML,
+      info_window: true
+    });
   }
 
   closeWindow() {
@@ -74,7 +57,7 @@ class App extends React.Component {
         <MenuList topChange={this.handleClick} />
         {this.state.info_window &&
           <InfoWindow
-            info={this.state.info_state}
+            info={infos[this.state.info_state - 1]}
             close={this.closeWindow} />
         }
       </div>
